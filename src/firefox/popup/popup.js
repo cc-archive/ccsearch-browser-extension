22bb4a15cff62e7733da137e0a64f2489342c7aa
Mayank Nader <mayank.nader11@gmail.com>
2019-06-05 06:53:08 +0530

    Add logic to show raw thumbnails

--- src/firefox/popup/popup.js
+++ src/firefox/popup/popup.js
@@ -4,6 +4,7 @@ const inputField = document.getElementById('section-search-input');
 const searchIcon = document.getElementById('search-icon');
 const errorMessage = document.getElementById('error-message');
 
+// Activate the click event on pressing enter.
 inputField.addEventListener('keydown', (event) => {
   if (event.keyCode === 13) {
     searchIcon.click();
@@ -16,4 +17,28 @@ searchIcon.addEventListener('click', () => {
     errorMessage.textContent = 'Please enter a search query';
   }
   console.log(inputText);
+
+  const url = `https://api.creativecommons.engineering/image/search?q=${inputText}&pagesize=50`;
+
+  fetch(url)
+    .then(data => data.json())
+    .then((res) => {
+      console.log(res);
+      const resultArray = res.results;
+      const thumbnails = [];
+      resultArray.forEach((element) => {
+        thumbnails.push(element.thumbnail);
+      });
+      console.log(thumbnails);
+      console.log(resultArray);
+      thumbnails.forEach((element) => {
+        const sectionContentParagraph = document.querySelector('.section-content p');
+        if (sectionContentParagraph) {
+          sectionContentParagraph.parentNode.removeChild(sectionContentParagraph);
+        }
+        const imgElement = document.createElement('img');
+        imgElement.setAttribute('src', element);
+        document.querySelector('.section-content').appendChild(imgElement);
+      });
+    });
 });
