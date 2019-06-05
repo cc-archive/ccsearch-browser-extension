4664239e075b791df07c68741c46012551ffa227
Mayank Nader <mayank.nader11@gmail.com>
2019-06-05 06:32:31 +0530

    Add search related eventlisteners and no-input error message logic

--- src/firefox/popup/popup.js
+++ src/firefox/popup/popup.js
@@ -1,3 +1,19 @@
-// check https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Debugging
-// for debugging instructions in firefox.
 console.log('hello from the extension!');
+
+const inputField = document.getElementById('section-search-input');
+const searchIcon = document.getElementById('search-icon');
+const errorMessage = document.getElementById('error-message');
+
+inputField.addEventListener('keydown', (event) => {
+  if (event.keyCode === 13) {
+    searchIcon.click();
+  }
+});
+
+searchIcon.addEventListener('click', () => {
+  const inputText = inputField.value;
+  if (inputText === '') {
+    errorMessage.textContent = 'Please enter a search query';
+  }
+  console.log(inputText);
+});
