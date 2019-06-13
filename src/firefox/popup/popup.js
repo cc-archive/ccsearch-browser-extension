c4ab60ba3a94a039ea99c5a8eb4e49b1a3d6a080
Mayank Nader <mayank.nader11@gmail.com>
2019-06-13 10:31:54 +0530

    Check if the image title is empty

--- src/firefox/popup/popup.js
+++ src/firefox/popup/popup.js
@@ -11,7 +11,10 @@ inputField.addEventListener('keydown', (event) => {
 
 // convert Unicode sequence To String. credit: https://stackoverflow.com/a/22021709/10425980
 function unicodeToString(string) {
-  return string.replace(/\\u[\dA-F]{4}/gi, match => String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16)));
+  if (typeof string !== 'undefined') {
+    return string.replace(/\\u[\dA-F]{4}/gi, match => String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16)));
+  }
+  return '';
 }
 
 searchIcon.addEventListener('click', () => {
