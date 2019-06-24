38a72d0e8e41356894e70d79b019fe6bcf94bf24
Mayank Nader <mayank.nader11@gmail.com>
2019-06-25 00:48:33 +0530

    Change some actions in searchIcon event listener to functions

--- src/firefox/popup/popup.js
+++ src/firefox/popup/popup.js
@@ -64,6 +64,34 @@ const backupProviderAPIQueryStrings = {
   'World Register of Marine Species': 'WoRMS',
 };
 
+// all the provider logo image file names
+const providerLogos = [
+  '500px_logo.png',
+  'animaldiversity_logo.png',
+  'brooklynmuseum_logo.png',
+  'behance_logo.svg',
+  'CAPL_logo.png',
+  'clevelandmuseum_logo.png',
+  'deviantart_logo.png',
+  'digitaltmuseum_logo.png',
+  'eol_logo.png',
+  'flickr_logo.png',
+  'floraon_logo.png',
+  'geographorguk_logo.gif',
+  'iha_logo.png',
+  'mccordmuseum_logo.png',
+  'met_logo.png',
+  'museumsvictoria_logo.svg',
+  'nypl_logo.svg',
+  'rawpixel_logo.png',
+  'rijksmuseum_logo.png',
+  'sciencemuseum_logo.svg',
+  'svgsilh_logo.png',
+  'thingiverse_logo.png',
+  'thorvaldsensmuseum_logo.png',
+  'WoRMS_logo.png',
+];
+
 // Activate the click event on pressing enter.
 inputField.addEventListener('keydown', (event) => {
   if (event.keyCode === 13) {
@@ -72,6 +100,14 @@ inputField.addEventListener('keydown', (event) => {
 });
 
 // Helper Functions
+
+// convert Unicode sequence To String. credit: https://stackoverflow.com/a/22021709/10425980
+function unicodeToString(string) {
+  if (typeof string !== 'undefined') {
+    return string.replace(/\\u[\dA-F]{4}/gi, match => String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16)));
+  }
+  return '';
+}
 
 function isObjectEmpty(obj) {
   return Object.keys(obj).length === 0 && obj.constructor === Object;
@@ -88,6 +124,141 @@ function getRequestUrl(
     return `https://api.creativecommons.engineering/image/search?q=${inputText}&page=${page}&pagesize=50&lt=${userSelectedUseCaseList}&provider=${userSelectedProvidersList}`;
   }
   return `https://api.creativecommons.engineering/image/search?q=${inputText}&page=${page}&pagesize=50&li=${userSelectedLicensesList}&provider=${userSelectedProvidersList}`;
+}
+
+function checkInputError(inputText) {
+  if (inputText === '') {
+    errorMessage.textContent = 'Please enter a search query';
+    throw new Error('Please enter a search query');
+  } else {
+    errorMessage.textContent = '';
+  }
+}
+
+function removeInitialContent() {
+  const sectionContentParagraph = document.querySelector('.section-content p');
+  if (sectionContentParagraph) {
+    sectionContentParagraph.parentNode.removeChild(sectionContentParagraph);
+  }
+}
+
+function removeOldSearchResults() {
+  // remove old images for a new search
+  const firstImgCol = document.querySelector('.section-content .first-col .images');
+  const secondImgCol = document.querySelector('.section-content .second-col .images');
+  const thirdImgCol = document.querySelector('.section-content .third-col .images');
+
+  firstImgCol.innerHTML = '';
+  secondImgCol.innerHTML = '';
+  thirdImgCol.innerHTML = '';
+}
+
+function showNoResultFoundMessage() {
+  const sectionContent = document.querySelector('.section-content');
+  const noResultFoundPara = document.createElement('p');
+  noResultFoundPara.textContent = 'No result Found. Please try a different search query.';
+  sectionContent.appendChild(noResultFoundPara);
+}
+
+function checkResultLength(resultArray) {
+  if (resultArray.length === 0) {
+    showNoResultFoundMessage();
+  }
+}
+
+function addThumbnailsToDOM(resultArray) {
+  let count = 1;
+  resultArray.forEach((element) => {
+    const thumbnail = element.thumbnail ? element.thumbnail : element.url;
+    const title = unicodeToString(element.title);
+    const { license } = element;
+    const licenseArray = license.split('-'); // split license in individual characteristics
+    const foreignLandingUrl = element.foreign_landing_url;
+    const { provider } = element;
+
+    // make an image element
+    const imgElement = document.createElement('img');
+    imgElement.setAttribute('src', thumbnail);
+
+    // make a span to hold the title
+    const spanTitleElement = document.createElement('span');
+    spanTitleElement.setAttribute('class', 'image-title');
+    spanTitleElement.setAttribute('title', title);
+    const imageTitleNode = document.createTextNode(title);
+
+    const foreignLandingLinkElement = document.createElement('a');
+    foreignLandingLinkElement.setAttribute('href', foreignLandingUrl);
+    foreignLandingLinkElement.setAttribute('target', '_blank');
+    foreignLandingLinkElement.setAttribute('class', 'foreign-landing-url');
+
+    const providerImageElement = document.createElement('img');
+    let providerLogoName;
+    for (let i = 0; i < providerLogos.length; i += 1) {
+      if (providerLogos[i].includes(provider)) {
+        providerLogoName = providerLogos[i];
+        break;
+      }
+    }
+    providerImageElement.setAttribute('src', `img/provider_logos/${providerLogoName}`);
+    providerImageElement.setAttribute('class', 'provider-image');
+
+    foreignLandingLinkElement.appendChild(providerImageElement);
+    foreignLandingLinkElement.appendChild(imageTitleNode);
+
+    spanTitleElement.appendChild(foreignLandingLinkElement);
+
+    // make a span to hold the license icons
+    const spanLicenseElement = document.createElement('span');
+    spanLicenseElement.setAttribute('class', 'image-license');
+
+    // make a link to license description
+    const licenseLinkElement = document.createElement('a');
+    licenseLinkElement.setAttribute('href', `https://creativecommons.org/licenses/${license}/2.0/`);
+    licenseLinkElement.setAttribute('target', '_blank'); // open link in new tab
+    licenseLinkElement.setAttribute('title', license); // open link in new tab
+
+    // Array to hold license image elements
+    const licenseIconElementsArray = [];
+
+    // Add the default cc icon
+    let licenseIconElement = document.createElement('img');
+    licenseIconElement.setAttribute('src', 'img/license_logos/cc_icon.svg');
+    licenseIconElement.setAttribute('alt', 'cc_icon');
+    licenseIconElementsArray.push(licenseIconElement);
+
+    // make and push license image elements
+    licenseArray.forEach((name) => {
+      licenseIconElement = document.createElement('img');
+      licenseIconElement.setAttribute('src', `img/license_logos/cc-${name}_icon.svg`);
+      licenseIconElement.setAttribute('alt', `cc-${name}_icon`);
+      licenseIconElementsArray.push(licenseIconElement);
+    });
+
+    licenseIconElementsArray.forEach((licenseIcon) => {
+      licenseLinkElement.appendChild(licenseIcon);
+    });
+    spanLicenseElement.appendChild(licenseLinkElement);
+
+    // make an div element to encapsulate image element
+    const divElement = document.createElement('div');
+    divElement.setAttribute('class', 'image');
+
+    divElement.appendChild(imgElement);
+    divElement.appendChild(spanTitleElement);
+    divElement.appendChild(spanLicenseElement);
+
+    // fill the grid
+    if (count === 1) {
+      document.querySelector('.section-content .first-col .images').appendChild(divElement);
+      count += 1;
+    } else if (count === 2) {
+      document.querySelector('.section-content .second-col .images').appendChild(divElement);
+      count += 1;
+    } else if (count === 3) {
+      document.querySelector('.section-content .third-col .images').appendChild(divElement);
+      count = 1;
+    }
+  });
 }
 
 function populateProviderList(providerAPIQuerystrings) {
@@ -289,39 +460,13 @@ filterApplyButton.addEventListener('click', () => {
   searchIcon.click();
 });
 
-// convert Unicode sequence To String. credit: https://stackoverflow.com/a/22021709/10425980
-function unicodeToString(string) {
-  if (typeof string !== 'undefined') {
-    return string.replace(/\\u[\dA-F]{4}/gi, match => String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16)));
-  }
-  return '';
-}
-
 searchIcon.addEventListener('click', () => {
   inputText = inputField.value;
   pageNo = 1;
 
-  if (inputText === '') {
-    errorMessage.textContent = 'Please enter a search query';
-    throw new Error('Please enter a search query');
-  } else {
-    errorMessage.textContent = '';
-  }
-
-  // remove initial content
-  const sectionContentParagraph = document.querySelector('.section-content p');
-  if (sectionContentParagraph) {
-    sectionContentParagraph.parentNode.removeChild(sectionContentParagraph);
-  }
-
-  // remove old images for a new search
-  const firstImgCol = document.querySelector('.section-content .first-col .images');
-  const secondImgCol = document.querySelector('.section-content .second-col .images');
-  const thirdImgCol = document.querySelector('.section-content .third-col .images');
-
-  firstImgCol.innerHTML = '';
-  secondImgCol.innerHTML = '';
-  thirdImgCol.innerHTML = '';
+  checkInputError(inputText);
+  removeInitialContent();
+  removeOldSearchResults();
 
   // enable spinner
   spinner.classList.add('spinner');
@@ -348,137 +493,8 @@ searchIcon.addEventListener('click', () => {
       const resultArray = res.results;
       console.log(resultArray);
 
-      let count = 1;
-
-      if (resultArray.length === 0) {
-        const sectionContent = document.querySelector('.section-content');
-        const noResultFoundPara = document.createElement('p');
-        noResultFoundPara.textContent = 'No result Found. Please try a different search query.';
-        sectionContent.appendChild(noResultFoundPara);
-      }
-
-      // all the provider logo image file names
-      const providerLogos = [
-        '500px_logo.png',
-        'animaldiversity_logo.png',
-        'brooklynmuseum_logo.png',
-        'behance_logo.svg',
-        'CAPL_logo.png',
-        'clevelandmuseum_logo.png',
-        'deviantart_logo.png',
-        'digitaltmuseum_logo.png',
-        'eol_logo.png',
-        'flickr_logo.png',
-        'floraon_logo.png',
-        'geographorguk_logo.gif',
-        'iha_logo.png',
-        'mccordmuseum_logo.png',
-        'met_logo.png',
-        'museumsvictoria_logo.svg',
-        'nypl_logo.svg',
-        'rawpixel_logo.png',
-        'rijksmuseum_logo.png',
-        'sciencemuseum_logo.svg',
-        'svgsilh_logo.png',
-        'thingiverse_logo.png',
-        'thorvaldsensmuseum_logo.png',
-        'WoRMS_logo.png',
-      ];
-
-      resultArray.forEach((element) => {
-        const thumbnail = element.thumbnail ? element.thumbnail : element.url;
-        const title = unicodeToString(element.title);
-        const { license } = element;
-        const licenseArray = license.split('-'); // split license in individual characteristics
-        const foreignLandingUrl = element.foreign_landing_url;
-        const { provider } = element;
-
-        // make an image element
-        const imgElement = document.createElement('img');
-        imgElement.setAttribute('src', thumbnail);
-
-        // make a span to hold the title
-        const spanTitleElement = document.createElement('span');
-        spanTitleElement.setAttribute('class', 'image-title');
-        spanTitleElement.setAttribute('title', title);
-        const imageTitleNode = document.createTextNode(title);
-
-        const foreignLandingLinkElement = document.createElement('a');
-        foreignLandingLinkElement.setAttribute('href', foreignLandingUrl);
-        foreignLandingLinkElement.setAttribute('target', '_blank');
-        foreignLandingLinkElement.setAttribute('class', 'foreign-landing-url');
-
-        const providerImageElement = document.createElement('img');
-        let providerLogoName;
-        for (let i = 0; i < providerLogos.length; i += 1) {
-          if (providerLogos[i].includes(provider)) {
-            providerLogoName = providerLogos[i];
-            break;
-          }
-        }
-        providerImageElement.setAttribute('src', `img/provider_logos/${providerLogoName}`);
-        providerImageElement.setAttribute('class', 'provider-image');
-
-        foreignLandingLinkElement.appendChild(providerImageElement);
-        foreignLandingLinkElement.appendChild(imageTitleNode);
-
-        spanTitleElement.appendChild(foreignLandingLinkElement);
-
-        // make a span to hold the license icons
-        const spanLicenseElement = document.createElement('span');
-        spanLicenseElement.setAttribute('class', 'image-license');
-
-        // make a link to license description
-        const licenseLinkElement = document.createElement('a');
-        licenseLinkElement.setAttribute(
-          'href',
-          `https://creativecommons.org/licenses/${license}/2.0/`,
-        );
-        licenseLinkElement.setAttribute('target', '_blank'); // open link in new tab
-        licenseLinkElement.setAttribute('title', license); // open link in new tab
-
-        // Array to hold license image elements
-        const licenseIconElementsArray = [];
-
-        // Add the default cc icon
-        let licenseIconElement = document.createElement('img');
-        licenseIconElement.setAttribute('src', 'img/license_logos/cc_icon.svg');
-        licenseIconElement.setAttribute('alt', 'cc_icon');
-        licenseIconElementsArray.push(licenseIconElement);
-
-        // make and push license image elements
-        licenseArray.forEach((name) => {
-          licenseIconElement = document.createElement('img');
-          licenseIconElement.setAttribute('src', `img/license_logos/cc-${name}_icon.svg`);
-          licenseIconElement.setAttribute('alt', `cc-${name}_icon`);
-          licenseIconElementsArray.push(licenseIconElement);
-        });
-
-        licenseIconElementsArray.forEach((licenseIcon) => {
-          licenseLinkElement.appendChild(licenseIcon);
-        });
-        spanLicenseElement.appendChild(licenseLinkElement);
-
-        // make an div element to encapsulate image element
-        const divElement = document.createElement('div');
-        divElement.setAttribute('class', 'image');
-
-        divElement.appendChild(imgElement);
-        divElement.appendChild(spanTitleElement);
-        divElement.appendChild(spanLicenseElement);
-
-        // fill the grid
-        if (count === 1) {
-          document.querySelector('.section-content .first-col .images').appendChild(divElement);
-          count += 1;
-        } else if (count === 2) {
-          document.querySelector('.section-content .second-col .images').appendChild(divElement);
-          count += 1;
-        } else if (count === 3) {
-          document.querySelector('.section-content .third-col .images').appendChild(divElement);
-          count = 1;
-        }
-      });
+      checkResultLength(resultArray);
+      addThumbnailsToDOM(resultArray);
     });
 });
 
@@ -567,7 +583,7 @@ async function nextRequest(page) {
 }
 
 // Trigger nextRequest when we reach bottom of the page
-// https://stackoverflow.com/a/10662576/10425980
+// credit: https://stackoverflow.com/a/10662576/10425980
 $(document).ready(() => {
   $(document).scroll(() => {
     if (processing) return false;
