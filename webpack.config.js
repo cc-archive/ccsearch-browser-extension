e292807ecf5c64339c015fb63de82f86d2e67bea
Mayank Nader <mayank.nader11@gmail.com>
2019-07-07 12:10:46 +0530

    install and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+console.log(path.resolve(__dirname, 'src/firefox/*'));
+console.log(path.resolve(__dirname, 'dist/firefox/'));
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './firefox/popup/popup.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'firefox/popup/popup.js',
+  },
+  plugins: [
+    new CopyPlugin([
+      {
+        from: 'firefox/*',
+        to: 'firefox/',
+        flatten: true,
+      },
+      {
+        from: 'firefox/icons/*',
+        to: 'firefox/icons/',
+        flatten: true,
+      },
+      {
+        from: 'firefox/popup/*',
+        to: 'firefox/popup/',
+        flatten: true,
+        ignore: ['popup.js'],
+      },
+      {
+        from: 'firefox/popup/img/*',
+        to: 'firefox/popup/img/',
+        flatten: true,
+      },
+      {
+        from: 'firefox/popup/img/license_logos/*',
+        to: 'firefox/popup/img/license_logos/',
+        flatten: true,
+      },
+      {
+        from: 'firefox/popup/img/provider_logos/*',
+        to: 'firefox/popup/img/provider_logos/',
+        flatten: true,
+      },
+    ]),
+  ],
+};
+
+// input, outpout, loaders, plugin
