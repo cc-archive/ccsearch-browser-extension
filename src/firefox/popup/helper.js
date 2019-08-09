288d536dda90d60fa95bcf5a327a446e02ee3ef4
Mayank Nader <mayank.nader11@gmail.com>
2019-08-09 07:20:34 +0530

    Write test for unicodeToString function

--- src/firefox/popup/helper.js
+++ src/firefox/popup/helper.js
@@ -1,6 +1,6 @@
 // convert Unicode sequence To String. credit: https://stackoverflow.com/a/22021709/10425980
 export function unicodeToString(string) {
-  if (typeof string !== 'undefined') {
+  if (typeof string === 'string') {
     return string.replace(/\\u[\dA-F]{4}/gi, match => String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16)));
   }
   return '';
--- tests/helper.test.js
+++ tests/helper.test.js
@@ -1,4 +1,4 @@
-import { isObjectEmpty } from '../src/firefox/popup/helper';
+import { isObjectEmpty, unicodeToString } from '../src/firefox/popup/helper';
 
 test('Testing isObjectEmpty', () => {
   const test1 = isObjectEmpty({});
@@ -6,4 +6,16 @@ test('Testing isObjectEmpty', () => {
 
   expect(test1).toBe(true);
   expect(test2).toBe(false);
+});
+
+test('Testing unicodeToString function', () => {
+  const test1 = unicodeToString(undefined);
+  const test2 = unicodeToString(null);
+  const test3 = unicodeToString(42);
+  const test4 = unicodeToString('good bye \u5c0f\u82d1');
+
+  expect(test1).toBe('');
+  expect(test2).toBe('');
+  expect(test3).toBe('');
+  expect(test4).toBe('good bye 小苑');
 });
