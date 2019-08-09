1d41ab2cf0a3dde030bee1a4bce23916d4285753
Mayank Nader <mayank.nader11@gmail.com>
2019-08-09 22:22:51 +0530

    Write test for getRequestUrl

--- tests/searchModule.test.js
+++ tests/searchModule.test.js
@@ -1,4 +1,4 @@
-import { checkInputError } from '../src/firefox/popup/searchModule';
+import { checkInputError, getRequestUrl } from '../src/firefox/popup/searchModule';
 
 test('testing checkInputError', () => {
   // error message placeholder
@@ -18,4 +18,34 @@ test('testing checkInputError', () => {
   // testing non-empty inputText
   checkInputError('hello', 'error-message');
   expect(document.getElementById('error-message').textContent).toEqual('');
+});
+
+test('testing getRequestUrl', () => {
+  const inputText = 'dogs';
+  let userSelectedUseCaseList = ['commercial'];
+  const userSelectedLicensesList = ['CC0'];
+  const userSelectedProvidersList = ['sciencemuseum'];
+  const page = 2;
+
+  const test1 = getRequestUrl(
+    inputText,
+    userSelectedUseCaseList,
+    userSelectedLicensesList,
+    userSelectedProvidersList,
+    page,
+  );
+  expect(test1).toMatch(/\?q=dogs&page=2&pagesize=20/);
+  expect(test1).toMatch(/lt=commercial/);
+
+  userSelectedUseCaseList = [];
+
+  const test2 = getRequestUrl(
+    inputText,
+    userSelectedUseCaseList,
+    userSelectedLicensesList,
+    userSelectedProvidersList,
+    page,
+  );
+  expect(test1).toMatch(/\?q=dogs&page=2&pagesize=20/);
+  expect(test2).toMatch(/li=CC0/);
 });
