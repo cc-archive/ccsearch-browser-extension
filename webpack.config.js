8d67c8ea12d7a9e40e1e6c9b69e72d83c5ba0f32
Mayank Nader <mayank.nader11@gmail.com>
2019-08-15 20:48:47 +0530

    Add error handling and enforce specification of a target while running webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
+if (!process.env.TARGET) {
+  throw Error("Please specify env var TARGET, 'chrome' or 'firefox'.");
+} else if (!(process.env.TARGET === 'chrome' || process.env.TARGET === 'firefox')) {
+  throw Error("TARGET can only be 'chrome' or 'firefox'.");
+} else {
+  console.info(`\x1b[1;32mBuilding for target ${process.env.TARGET}...\x1b[m`);
+}
+
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry: {
@@ -63,5 +71,3 @@ module.exports = {
     ]),
   ],
 };
-
-// input, outpout, loaders, plugin
