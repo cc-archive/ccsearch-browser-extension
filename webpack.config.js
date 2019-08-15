8b8ca202e533e46cc2cfc8b445efc031d3f4d12a
Mayank Nader <mayank.nader11@gmail.com>
2019-08-16 01:54:51 +0530

    Specify opera as a valid option for target

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,17 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 if (!process.env.TARGET) {
-  throw Error("Please specify env var TARGET, 'chrome' or 'firefox'.");
-} else if (!(process.env.TARGET === 'chrome' || process.env.TARGET === 'firefox')) {
-  throw Error("TARGET can only be 'chrome' or 'firefox'.");
+  throw Error("Please specify env var TARGET, 'chrome', 'firefox' or 'opera'.");
+} else if (
+  !(
+    process.env.TARGET === 'chrome'
+    || process.env.TARGET === 'firefox'
+    || process.env.TARGET === 'opera'
+  )
+) {
+  throw Error("TARGET can only be 'chrome', 'firefox' or 'opera'.");
 } else {
-  console.info(`\x1b[1;32mBuilding for target ${process.env.TARGET}...\x1b[m`);
+  console.info(`\x1b[1;32mBuilding for ${process.env.TARGET}...\x1b[m`);
 }
 
 module.exports = {
