585b92a051925cb12c8f81035e2628f8dc68bb74
SorielV <xxsorielxx@gmail.com>
2019-09-29 12:19:27 +0000

    Improvement alpha-numeric validation

--- src/popup/searchModule.js
+++ src/popup/searchModule.js
@@ -13,7 +13,7 @@ export function checkInputError(inputText) {
     showNotification('No search query provided', 'negative', 'snackbar-bookmarks');
     // to stop further js execution
     throw new Error('No search query provided');
-  } else if (!/^[A-Za-z0-9 ]+$/.test(inputText)) {
+  } else if (!/^[A-Za-z0-9]( ?[A-Za-z0-9])*$/.test(inputText)) {
     showNotification('No valid search query provided, search query must be alphanumeric', 'negative', 'snackbar-bookmarks');
     // to stop further js execution
     throw new Error('No valid search query provided');
