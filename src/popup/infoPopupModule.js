9ff4d940cf108db36663ccb9f3f0a61154ac527c
Gaurav Ahlawat <gauravahlawat81@gmail.com>
2020-01-30 22:43:19 +0530

    Fix #106: render the creator name as plain text when creator URL is not available(#115)

--- src/popup/infoPopupModule.js
+++ src/popup/infoPopupModule.js
@@ -131,6 +131,14 @@ function getTumblrShareLink(sourceLink, imageLink) {
 }
 
 function getPopupCreatorChildNode(creatorUrl, creator) {
+  // This following code fixes the issue #106
+  // if the creatorURL doesn't exist, we do not create
+  // a anchor tag, instead we just create a paragraph tag
+  if (creatorUrl === '#') {
+    const paragraph = document.createElement('p');
+    paragraph.textContent = creator;
+    return paragraph;
+  }
   const link = document.createElement('a');
   link.href = creatorUrl;
   link.target = '_blank';
