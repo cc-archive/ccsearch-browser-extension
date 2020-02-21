152fa32f6d95d9690e46ad1a3d6e7d6ee7d4dc64
mayank nader <mayank.nader11@gmail.com>
2020-02-21 19:40:36 +0530

    remove lint errors from tests

--- tests/spinner.test.js
+++ tests/spinner.test.js
@@ -3,20 +3,20 @@ import { getSpinnerDiv } from '../src/popup/spinner';
 test('Checking spinner markup', () => {
   const spinnerMarkupForBottom = getSpinnerDiv('for-bottom');
 
-  expect(spinnerMarkupForBottom.classList).toContain('spinner')
-  expect(spinnerMarkupForBottom.classList).toContain('spinner-for-bottom')
-  expect(spinnerMarkupForBottom.childElementCount).toBe(3)
-  let spinnerForBottomChildNodes = spinnerMarkupForBottom.childNodes
-  for (let i = 0; i < 3; i++) {
+  expect(spinnerMarkupForBottom.classList).toContain('spinner');
+  expect(spinnerMarkupForBottom.classList).toContain('spinner-for-bottom');
+  expect(spinnerMarkupForBottom.childElementCount).toBe(3);
+  const spinnerForBottomChildNodes = spinnerMarkupForBottom.childNodes;
+  for (let i = 0; i < 3; i += 1) {
     expect(spinnerForBottomChildNodes[i].classList).toContain(`bounce${i + 1}`);
   }
 
   const spinnerMarkupOriginal = getSpinnerDiv('original');
-  expect(spinnerMarkupOriginal.classList).toContain('spinner')
-  expect(spinnerMarkupOriginal.classList).toContain('spinner-original')
-  expect(spinnerMarkupOriginal.childElementCount).toBe(3)
-  let spinnerOriginalChildNodes = spinnerMarkupOriginal.childNodes
-  for (let i = 0; i < 3; i++) {
+  expect(spinnerMarkupOriginal.classList).toContain('spinner');
+  expect(spinnerMarkupOriginal.classList).toContain('spinner-original');
+  expect(spinnerMarkupOriginal.childElementCount).toBe(3);
+  const spinnerOriginalChildNodes = spinnerMarkupOriginal.childNodes;
+  for (let i = 0; i < 3; i += 1) {
     expect(spinnerOriginalChildNodes[i].classList).toContain(`bounce${i + 1}`);
   }
 });
