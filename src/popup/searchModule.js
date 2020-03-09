a9927b878f4e898f5e46d66af451c8df661a027d
kss682 <kss682@gmail.com>
2020-03-10 00:52:22 +0530

    Validation Error changed as per v1 schema

--- src/popup/searchModule.js
+++ src/popup/searchModule.js
@@ -105,8 +105,9 @@ function appendToGrid(msnry, fragment, divs, grid) {
 }
 
 // TODO: be more specific
+// Object.prototype.hasOwnProperty.call(apiResponse, 'validation_error'
 export function checkValidationError(apiResponse) {
-  if (Object.prototype.hasOwnProperty.call(apiResponse, 'validation_error')) {
+  if (apiResponse.result_count == 0) {
     removeLoadMoreButton(elements.loadMoreButtonWrapper);
     elements.gridPrimary.setAttribute('style', 'position: relative; height: 0px;');
     showNotification('Not a valid search query', 'negative', 'snackbar-bookmarks');
@@ -117,10 +118,10 @@ export function checkValidationError(apiResponse) {
   if (apiResponse.error_type === 'InputError') {
     removeLoadMoreButton(elements.loadMoreButtonWrapper);
     elements.gridPrimary.setAttribute('style', 'position: relative; height: 0px;');
-    showNotification('Not a valid search query', 'negative', 'snackbar-bookmarks');
+    showNotification('Input Error', 'negative', 'snackbar-bookmarks');
     removeLoaderAnimation();
     restoreInitialContent('primary');
-    throw new Error('Not valid search query');
+    throw new Error('Input Error');
   }
 }
 
