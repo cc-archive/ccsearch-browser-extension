703452e8608c4ef1f54e67d838fb6e9c3ae1a55d
Vivek Kumar <35863227+techievivek@users.noreply.github.com>
2020-04-22 23:35:23 +0530

    fix #143: Add a landing page for the browser extension (#205)

---
+++ docs/loadassets.js
@@ -0,0 +1,26 @@
+const getFullyQualifiedUrl = (path, version) => {
+  let baseUrl = 'https://unpkg.com/@creativecommons/vocabulary';
+  if (version) {
+    baseUrl = `${baseUrl}@${version}`;
+  }
+  return `${baseUrl}/${path}`;
+};
+
+const patchAssetIntoDom = (asset, version = null) => {
+  const ajax = new XMLHttpRequest();
+  ajax.open('GET', getFullyQualifiedUrl(asset, version), true);
+  ajax.onload = () => {
+    const div = document.createElement('div');
+    // Render SVG in the page
+    div.innerHTML = ajax.responseText;
+    div.style.display = 'none';
+    document.body.insertBefore(div, document.body.childNodes[0]);
+  };
+
+  ajax.send();
+};
+
+patchAssetIntoDom('/assets/github_corner.svg');
+patchAssetIntoDom('/assets/logos/cc/logomark.svg');
+patchAssetIntoDom('/assets/logos/products/search.svg');
+patchAssetIntoDom('/assets/logos/cc/letterheart.svg');
