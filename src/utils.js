2ae5fed5d7ea1d1e2e3ad7ba1223d9182caaf3d9
Mayank Nader <mayank.nader11@gmail.com>
2020-06-22 19:14:36 +0530

    update classes of bookmarks section initial content

--- src/utils.js
+++ src/utils.js
@@ -47,7 +47,7 @@ export function restoreInitialContent(context) {
     if (context === 'primary') {
       initialInfoElement = `<p></p>`;
     } else if (context === 'bookmarks') {
-      initialInfoElement = `<p class="vocab paragraph inherit-colored negative-indicating initial-info bookmarks__initial-info">
+      initialInfoElement = `<p class="initial-info bookmarks__initial-info has-text-tomato">
               No Bookmarks yet
             </p>`;
     }
