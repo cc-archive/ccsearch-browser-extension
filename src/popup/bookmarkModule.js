4242776aca72d8f3bdfd762cc6cc986822f873f1
Mayank Nader <mayank.nader11@gmail.com>
2020-08-06 00:03:48 +0530

    vocab-integration: use vocabulary image in bookmark section
    This commit handles only the markup of the component. The workflow of selecting/deselecting and deletion is also to be modified to work with the new component.

--- src/popup/bookmarkModule.js
+++ src/popup/bookmarkModule.js
@@ -152,55 +152,44 @@ function addBookmarkThumbnailsToDOM(bookmarksObject, bookmarkImageIds) {
     imgElement.setAttribute('id', imageId);
 
     // make select button
-    const selectCheckboxElement = document.createElement('span');
-    selectCheckboxElement.setAttribute('class', 'bookmark-select');
-    const selectCheckbox = document.createElement('input');
-    selectCheckbox.setAttribute('type', 'checkbox');
-    selectCheckbox.setAttribute('data-image-id', imageId);
-    selectCheckbox.setAttribute('title', 'Select Image');
-    selectCheckbox.setAttribute('data-image-thumbnail', thumbnail);
-    selectCheckbox.setAttribute('data-image-license', license);
-    selectCheckbox.classList.add('select-checkbox');
-    selectCheckbox.classList.add('margin-right-smaller');
-    selectCheckboxElement.appendChild(selectCheckbox);
+    // const selectCheckboxElement = document.createElement('span');
+    // selectCheckboxElement.setAttribute('class', 'bookmark-select');
+    // const selectCheckbox = document.createElement('input');
+    // selectCheckbox.setAttribute('type', 'checkbox');
+    // selectCheckbox.setAttribute('data-image-id', imageId);
+    // selectCheckbox.setAttribute('title', 'Select Image');
+    // selectCheckbox.setAttribute('data-image-thumbnail', thumbnail);
+    // selectCheckbox.setAttribute('data-image-license', license);
+    // selectCheckbox.classList.add('select-checkbox');
+    // selectCheckbox.classList.add('margin-right-smaller');
+    // selectCheckboxElement.appendChild(selectCheckbox);
 
-    // make a span to hold the license icons
-    const spanLicenseElement = document.createElement('span');
-    spanLicenseElement.setAttribute('class', 'image-license');
-
-    // make a link to license description
-    const licenseLinkElement = document.createElement('a');
-    licenseLinkElement.setAttribute('href', `https://creativecommons.org/licenses/${license}/2.0/`);
-    licenseLinkElement.setAttribute('target', '_blank'); // open link in new tab
-    licenseLinkElement.setAttribute('title', license); // open link in new tab
+    const licenseDiv = document.createElement('div');
+    licenseDiv.classList.add('image-icons');
 
     // Array to hold license image elements
     const licenseIconElementsArray = [];
 
     // Add the default cc icon
-    let licenseIconElement = document.createElement('img');
-    licenseIconElement.setAttribute('src', 'img/license_logos/cc_icon.svg');
-    licenseIconElement.setAttribute('alt', 'cc_icon');
+    let licenseIconElement = document.createElement('i');
+    licenseIconElement.classList.add('icon', 'has-background-white', 'cc-logo');
     licenseIconElementsArray.push(licenseIconElement);
 
     // make and push license image elements
     licenseArray.forEach(name => {
       const lowerCaseName = `${name}`.toLowerCase();
-      licenseIconElement = document.createElement('img');
-      licenseIconElement.setAttribute('src', `img/license_logos/cc-${lowerCaseName}_icon.svg`);
-      licenseIconElement.setAttribute('alt', `cc-${lowerCaseName}_icon`);
+      licenseIconElement = document.createElement('i');
+      licenseIconElement.classList.add('icon', 'has-background-white', `cc-${lowerCaseName}`);
       licenseIconElementsArray.push(licenseIconElement);
     });
 
     licenseIconElementsArray.forEach(licenseIcon => {
-      licenseLinkElement.appendChild(licenseIcon);
+      licenseDiv.appendChild(licenseIcon);
     });
-
-    spanLicenseElement.appendChild(licenseLinkElement);
 
     // make a div element to encapsulate image element
     const divElement = document.createElement('div');
-    divElement.setAttribute('class', 'image');
+    divElement.classList.add('image', 'is-compact');
     divElement.id = `id_${imageId}`; // used for searching image div element
 
     // adding event listener to open popup.
@@ -213,8 +202,8 @@ function addBookmarkThumbnailsToDOM(bookmarksObject, bookmarkImageIds) {
     });
 
     divElement.appendChild(imgElement);
-    divElement.appendChild(selectCheckboxElement);
-    divElement.appendChild(spanLicenseElement);
+    // divElement.appendChild(selectCheckboxElement);
+    divElement.appendChild(licenseDiv);
 
     // div to act as grid itemj
     const gridItemDiv = document.createElement('div');
@@ -229,35 +218,35 @@ function addBookmarkThumbnailsToDOM(bookmarksObject, bookmarkImageIds) {
     // Add onClick event to all the checkboxes
 
     // Get checkbox data from DOM
-    const checkbox = elements.selectCheckboxes[elements.selectCheckboxes.length - 1];
+    // const checkbox = elements.selectCheckboxes[elements.selectCheckboxes.length - 1];
 
     // Initiate isChecked property of checkbox and update bookmarkDOM
-    checkbox.isChecked = false;
-    bookmarkDOM[checkbox.dataset.imageId] = checkbox;
+    // checkbox.isChecked = false;
+    // bookmarkDOM[checkbox.dataset.imageId] = checkbox;
     // console.log(bookmarkDOM);
 
     selectedBookmarks = 0;
     elements.buttonSelectAllCheckbox[0].innerText = 'Select All';
 
     // Add click function to keep checkbox data in sync with DOM
-    checkbox.addEventListener('click', () => {
-      // Check wheather the checkbox is already checked or not
-      if (checkbox.isChecked) {
-        checkbox.parentElement.removeAttribute('style');
-        selectedBookmarks -= 1;
-      } else {
-        checkbox.parentElement.setAttribute('style', 'opacity : 1');
-        selectedBookmarks += 1;
-      }
-      checkbox.isChecked = !checkbox.isChecked; // Update isChecked Property in checkbox
+    // checkbox.addEventListener('click', () => {
+    //   // Check wheather the checkbox is already checked or not
+    //   if (checkbox.isChecked) {
+    //     checkbox.parentElement.removeAttribute('style');
+    //     selectedBookmarks -= 1;
+    //   } else {
+    //     checkbox.parentElement.setAttribute('style', 'opacity : 1');
+    //     selectedBookmarks += 1;
+    //   }
+    //   checkbox.isChecked = !checkbox.isChecked; // Update isChecked Property in checkbox
 
-      // Update SelectAll Button
-      if (selectedBookmarks === 0) {
-        elements.buttonSelectAllCheckbox[0].innerText = 'Select All';
-      } else if (selectedBookmarks > 0) {
-        elements.buttonSelectAllCheckbox[0].innerText = 'Deselect All';
-      }
-    });
+    //   // Update SelectAll Button
+    //   if (selectedBookmarks === 0) {
+    //     elements.buttonSelectAllCheckbox[0].innerText = 'Select All';
+    //   } else if (selectedBookmarks > 0) {
+    //     elements.buttonSelectAllCheckbox[0].innerText = 'Deselect All';
+    //   }
+    // });
   });
 }
 
