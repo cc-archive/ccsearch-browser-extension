36d99b8fdf2fbd8f233f45dddf7fea1374928606
Mayank Nader <mayank.nader11@gmail.com>
2020-08-20 17:13:06 +0530

    ammend tests to reflect changes done to showNotification function

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,21 +1,30 @@
 import { showNotification, removeNode } from '../src/utils';
 
 test('testing showNotification', () => {
-  document.body.innerHTML = '<div id="snackbar-bookmarks" class="snackbar"></div>';
+  document.body.innerHTML = `<div class="notification-wrapper notification--extension-popup display-none">
+    <div class="notification is-compact">
+      <span class="notification-container">
+        <p>This is a title of a message</p>
+      </span>
+    </div>
+  </div>`;
 
   jest.useFakeTimers();
-  showNotification('test', 'positive', 'snackbar-bookmarks');
-  const snackbar = document.getElementById('snackbar-bookmarks');
-  expect(snackbar.innerText).toBe('test');
-  expect(snackbar.classList).toContain('snackbar-positive');
-  expect(snackbar.classList).toContain('show');
+  showNotification('test', 'positive', 'notification--extension-popup');
+  const notificationWrapperDiv = document.getElementsByClassName('notification--extension-popup')[0];
+  const notificationContainer = notificationWrapperDiv.getElementsByClassName('notification-container')[0];
+  const notificationPara = notificationContainer.getElementsByTagName('p')[0];
+  expect(notificationPara.innerText).toBe('test');
+  expect(notificationWrapperDiv.classList).not.toContain('display-none');
+  expect(notificationContainer.classList).toContain('has-background-success-light');
   setTimeout(() => {
-    expect(snackbar.classList).not.toContain('show');
+    // expect(notificationWrapperDiv.classList).not.toContain('show');
+    expect(notificationWrapperDiv.classList).toContain('display-none');
   }, 1200);
   jest.runAllTimers();
 
-  showNotification('test', 'negative', 'snackbar-bookmarks');
-  expect(snackbar.classList).toContain('snackbar-negative');
+  showNotification('test', 'negative', 'notification--extension-popup');
+  expect(notificationContainer.classList).toContain('has-background-danger-light');
 });
 
 test('testing removeNode', () => {
