f3cff0c6a1230aec2d61043f37ba35657354e230
Mayank Nader <mayank.nader11@gmail.com>
2020-09-01 03:04:42 +0530

    run filter migration only once in production

--- src/popup/popup.utils.js
+++ src/popup/popup.utils.js
@@ -1,64 +1,70 @@
 export default function generateNewStorageSchemaForFilters() {
-  const aspectRatioFilter = {
-    tall: false,
-    wide: false,
-    square: false,
-  };
+  chrome.storage.sync.get({ filterMigration: false }, items => {
+    if (!items.filterMigration) {
+      const aspectRatioFilter = {
+        tall: false,
+        wide: false,
+        square: false,
+      };
 
-  const imageSizeFilter = {
-    small: false,
-    medium: false,
-    large: false,
-  };
+      const imageSizeFilter = {
+        small: false,
+        medium: false,
+        large: false,
+      };
 
-  const imageTypeFilter = {
-    photograph: false,
-    illustration: false,
-    digitized_artwork: false,
-  };
+      const imageTypeFilter = {
+        photograph: false,
+        illustration: false,
+        digitized_artwork: false,
+      };
 
-  const fileTypeFilter = {
-    jpeg: false,
-    png: false,
-    gif: false,
-    svg: false,
-  };
+      const fileTypeFilter = {
+        jpeg: false,
+        png: false,
+        gif: false,
+        svg: false,
+      };
 
-  const licenseFilter = {
-    CC0: false,
-    PDM: false,
-    BY: false,
-    'BY-SA': false,
-    'BY-NC': false,
-    'BY-ND': false,
-    'BY-NC-SA': false,
-    'BY-NC-ND': false,
-  };
+      const licenseFilter = {
+        CC0: false,
+        PDM: false,
+        BY: false,
+        'BY-SA': false,
+        'BY-NC': false,
+        'BY-ND': false,
+        'BY-NC-SA': false,
+        'BY-NC-ND': false,
+      };
 
-  const useCaseFilter = {
-    commercial: false,
-    modification: false,
-  };
+      const useCaseFilter = {
+        commercial: false,
+        modification: false,
+      };
 
-  const showMatureContentFilter = {
-    mature: false,
-  };
+      const showMatureContentFilter = {
+        mature: false,
+      };
 
-  const sourceFilter = {};
+      const sourceFilter = {};
 
-  chrome.storage.sync.set(
-    {
-      useCaseFilter,
-      licenseFilter,
-      imageTypeFilter,
-      fileTypeFilter,
-      aspectRatioFilter,
-      imageSizeFilter,
-      sourceFilter,
-      showMatureContentFilter,
-    },
-    () => {
-      console.log('done');
-    },
-  );
+      chrome.storage.sync.set(
+        {
+          useCaseFilter,
+          licenseFilter,
+          imageTypeFilter,
+          fileTypeFilter,
+          aspectRatioFilter,
+          imageSizeFilter,
+          sourceFilter,
+          showMatureContentFilter,
+          filterMigration: true,
+        },
+        () => {
+          console.log('done');
+        },
+      );
+    }
+  });
 }
+
