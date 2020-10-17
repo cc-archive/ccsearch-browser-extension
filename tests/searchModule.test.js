d9a12c801172b3639b1b310fd014aa125b527c57
Mayank Nader <mayank.nader11@gmail.com>
2020-10-18 00:35:19 +0530

    remove redundant tests

--- tests/searchModule.test.js
+++ tests/searchModule.test.js
@@ -1,10 +1,10 @@
 // import { JestEnvironment } from '@jest/environment';
-import { checkInputError, getRequestUrl } from '../src/popup/searchModule';
+import { checkInputError } from '../src/popup/searchModule';
 import * as utils from '../src/utils';
 
-utils.showNotification = jest.fn();
-
 test('testing checkInputError', () => {
+  utils.showNotification = jest.fn();
+
   // testing checkInputError with empty search query
   expect(() => {
     checkInputError('');
@@ -24,53 +24,4 @@ test('testing checkInputError', () => {
 
   // showNotification should be called only in case of empty search query
   expect(utils.showNotification).toHaveBeenCalledTimes(1);
-});
-
-test('testing getRequestUrl', () => {
-  const inputText = 'dogs';
-  let userSelectedUseCaseList = ['commercial'];
-  const userSelectedLicensesList = ['CC0'];
-  const userSelectedSourcesList = ['sciencemuseum'];
-  const userSelectedFileTypeList = ['jpeg', 'gif'];
-  let userSelectedImageTypeList = ['illustration'];
-  const userSelectedImageSizeList = ['small'];
-  const userSelectedAspectRatioList = ['square'];
-  const page = 2;
-
-  const urlTest1 = getRequestUrl(
-    inputText,
-    userSelectedUseCaseList,
-    userSelectedLicensesList,
-    userSelectedSourcesList,
-    userSelectedFileTypeList,
-    userSelectedImageTypeList,
-    userSelectedImageSizeList,
-    userSelectedAspectRatioList,
-    page,
-    true, // enableMatureContent
-  );
-  expect(urlTest1).toMatch(/\?q=dogs&page=2&page_size=20/);
-  expect(urlTest1).toMatch(/license_type=commercial/);
-  expect(urlTest1).toMatch(/extension=jpeg,gif&categories=illustration&size=small&aspect_ratio=square&mature=true/);
-
-  userSelectedUseCaseList = [];
-  userSelectedImageTypeList = ['illustration', 'digitized_artworks'];
-
-  const urlTest2 = getRequestUrl(
-    inputText,
-    userSelectedUseCaseList,
-    userSelectedLicensesList,
-    userSelectedSourcesList,
-    userSelectedFileTypeList,
-    userSelectedImageTypeList,
-    userSelectedImageSizeList,
-    userSelectedAspectRatioList,
-    page,
-    false, // enableMatureContent
-  );
-  expect(urlTest2).toMatch(/\?q=dogs&page=2&page_size=20/);
-  expect(urlTest2).toMatch(/license=CC0/);
-  expect(urlTest2).toMatch(
-    /extension=jpeg,gif&categories=illustration,digitized_artworks&size=small&aspect_ratio=square&mature=false/,
-  );
 });
